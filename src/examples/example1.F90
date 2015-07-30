eb086184f68c3be59bf1ccb66f0276afeb1aa179
Miro ILIAS <Miroslav.Ilias@umb.sk>
2015-07-30 20:36:51 +0200

    saving changes

--- src/examples/example1.F90
+++ src/examples/example1.F90
@@ -1,14 +1,16 @@
  module example_data_cfg
- ! module for storing/sharing data read from dedicated text input file
-  character*50 :: title_text
-  integer :: print_level
+ ! module for storing/sharing data which are read from dedicated text input file
+  character*50 :: title_text, title_text2
+  integer :: print_level, print_level2
   character*20 :: matrix_file_name
-  logical :: do_dsyevr=.false.
+  logical :: do_this=.false.
+  integer :: nelec_f1(16)
+  real*8  :: factor1,factor21,factor22
  end module example_data_cfg
 
  subroutine read_input_file
+ ! open and read the input file
   use input_reader
-  use example_data_cfg
   implicit none
   logical :: input_found
   integer :: lucmd = 5
@@ -18,6 +20,7 @@
   if (.not.input_found) then
    stop "input file not found !"
   endif
+  ! routine from src/lib/
   call read_menu_input(inpfile, lucmd, 'ALL', input_found)
  end subroutine read_input_file
 
@@ -28,38 +31,57 @@
   character(kw_length), intent(in) :: kw_section
   select case (kw_section)
    case ('**DIAG ')
-    call read_example_input(word, kw_section)
+    call read_example_input_diag(word, kw_section)
+   case ('**SECT2')
+    call read_example_input_sect2(word, kw_section)
    case default
-!          activate as soon as everything is merged to here
-!          call quit('section '//kw_section//' not recognized')
+   ! activate as soon as everything is merged to here
+    call quit('section '//kw_section//' not recognized')
   end select
  end subroutine
 
- subroutine read_example_input(word, kw_section)
+ subroutine read_example_input_diag(word, kw_section)
+ ! read input data of the **DIAG section
   use input_reader
   use example_data_cfg
   implicit none
   character(kw_length), intent(in) :: word
   character(kw_length), intent(in) :: kw_section
   call reset_available_kw_list()
-
   if (kw_matches(word, '.TITLE ')) then
    call kw_read(word, title_text)
   end if
-
   if (kw_matches(word, '.PRINT ')) then
    call kw_read(word, print_level)
   end if
-
   if (kw_matches(word, '.MTXFIL')) then
    call kw_read(word, matrix_file_name)
   end if
-
-  if (kw_matches(word, '.DSYEVR')) then
-   do_dsyevr = .true.
+  if (kw_matches(word, '.DOTHIS')) then
+   do_this = .true.
   end if
   call check_whether_kw_found(word, kw_section)
-end subroutine
+ end subroutine
+
+ subroutine read_example_input_sect2(word, kw_section)
+ ! read input data of the **DIAG section
+  use input_reader
+  use example_data_cfg
+  implicit none
+  character(kw_length), intent(in) :: word
+  character(kw_length), intent(in) :: kw_section
+  call reset_available_kw_list()
+  if (kw_matches(word, '.TITLE ')) then
+   call kw_read(word, title_text2)
+  end if
+  if (kw_matches(word, '.PRINT ')) then
+   call kw_read(word, print_level2)
+  end if
+  if (kw_matches(word, '.FACT1 ')) then
+   call kw_read(word,factor1)
+  end if
+  call check_whether_kw_found(word, kw_section)
+ end subroutine
 
  subroutine QUIT(TEXT)
   CHARACTER TEXT*(*)
@@ -70,5 +92,12 @@ end subroutine
  program Test_input_reader_1
   use example_data_cfg
   call read_input_file
-  print *,'Test_input_reader_1'
+  print *,title_text
+  print *,title_text2
+  print *,matrix_file_name
+  print *,print_level
+  print *,do_this
+  print *,factor1
+
+  print *,'Hello World!'
  end program
---
+++ test.py
@@ -0,0 +1,114 @@
+import os
+import sys
+import subprocess
+import shlex
+import shutil
+import sys
+import time
+import datetime
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+# ------------------------------------------------------------------------------
+
+
+# we do not use the nicer sys.version_info.major
+# for compatibility with Python < 2.7
+if sys.version_info[0] > 2:
+    import urllib.request
+
+    class URLopener(urllib.request.FancyURLopener):
+        def http_error_default(self, url, fp, errcode, errmsg, headers):
+            sys.stderr.write("ERROR: could not fetch %s\n" % url)
+            sys.exit(-1)
+else:
+    import urllib
+
+    class URLopener(urllib.FancyURLopener):
+        def http_error_default(self, url, fp, errcode, errmsg, headers):
+            sys.stderr.write("ERROR: could not fetch %s\n" % url)
+            sys.exit(-1)
+
+# ------------------------------------------------------------------------------
+
+
+def fetch_url(src, dst):
+    """
+    Fetch file from URL src and save it to dst.
+    """
+    dirname = os.path.dirname(dst)
+    if dirname != '':
+        if not os.path.isdir(dirname):
+            os.makedirs(dirname)
+
+    opener = URLopener()
+    opener.retrieve(src, dst)
+
+# ------------------------------------------------------------------------------
+
+
+def exe(command):
+    """
+    Executes command and returns string representations of stdout and stderr captured from the console.
+    When universal_newlines=True stdout and stderr are opened in text mode.
+    Otherwise, they are opened in binary mode. In that case captured stdout and stderr
+    are not strings and Python 3 throws type error when compared against strings later in tests.
+    Note:
+    This feature is only available if Python is built with universal newline support (the default).
+    Also, the newlines attribute of the file objects stdout, stdin and stderr are not updated by the
+    communicate() method.
+    See https://docs.python.org/2/library/subprocess.html
+    """
+    stdout, stderr = subprocess.Popen(shlex.split(command),
+                                      stdout=subprocess.PIPE,
+                                      stderr=subprocess.PIPE,
+                                      universal_newlines=True).communicate()
+
+    if stderr:
+        sys.stderr.write(stderr)
+
+    return stdout, stderr
+
+# ------------------------------------------------------------------------------
+
+
+def configure_build_and_exe(name, setup_command):
+
+    stamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d-%H-%M-%S')
+
+ #   os.chdir(os.path.join(HERE, name, 'cmake'))
+ #   shutil.copy(os.path.join('..', '..', '..', 'update.py'), 'update.py')
+ #   if not os.path.exists('lib'):
+ #       os.makedirs('lib')
+ #   shutil.copy(os.path.join('..', '..', '..', 'lib', 'config.py'), 'lib')
+ #   fetch_url(src='https://github.com/docopt/docopt/raw/master/docopt.py',
+ #             dst='lib/docopt.py')
+ #   stdout, stderr = exe('python update.py ..')
+    os.chdir(os.path.join(HERE, name))
+
+    if sys.platform == 'win32':
+        setup_command += ' --generator="MinGW Makefiles"'
+
+    setup_command += ' build-%s' % stamp
+
+    stdout, stderr = exe(setup_command)
+
+    os.chdir(os.path.join(HERE, name, 'build-%s' % stamp))
+
+    if sys.platform == 'win32':
+        stdout, stderr = exe('mingw32-make')
+        stdout, stderr = exe('bin\\\example.exe')
+    else:
+        stdout, stderr = exe('make')
+        stdout, stderr = exe('./bin/example')
+
+    return stdout, stderr
+
+# ------------------------------------------------------------------------------
+
+
+def test_examples():
+    stdout, stderr = configure_build_and_exe('', 'python setup.py --fc=gfortran')
+    assert 'Hello World!' in stdout
+
+# ------------------------------------------------------------------------------
